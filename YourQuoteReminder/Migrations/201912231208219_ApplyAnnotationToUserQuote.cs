611716c43cf55aeaa1103f8477ae204a2a1e172e
MahdiDroid <mehdi.mhmvd@gmail.com>
2019-12-23 16:12:00 +0300

    added anotation

---
+++ YourQuoteReminder/Migrations/201912231208219_ApplyAnnotationToUserQuote.cs
@@ -0,0 +1,18 @@
+﻿namespace YourQuoteReminder.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ApplyAnnotationToUserQuote : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Quotes", "UserQuote", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Quotes", "UserQuote", c => c.String());
+        }
+    }
+}
--- YourQuoteReminder/Models/Quote.cs
+++ YourQuoteReminder/Models/Quote.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace YourQuoteReminder.Models
 {
     public class Quote
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string UserQuote { get; set; }
         public DateTime CreatedDateTime { get; set; }
 
