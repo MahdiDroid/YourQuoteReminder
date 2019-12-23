8e0e0b5fe22fbb86b8c121b4301ec5f6963fc74a
MahdiDroid <mehdi.mhmvd@gmail.com>
2019-12-23 14:22:15 +0300

    db context class added

---
+++ YourQuoteReminder/QuoteDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using YourQuoteReminder.Models;
+
+namespace YourQuoteReminder
+{
+    public class QuoteDbContext :DbContext 
+    {
+        public DbSet<Quote> Quotes { get; set; }
+    }
+}
