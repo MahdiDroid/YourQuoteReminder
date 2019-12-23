626f371539cae75d8097f8e49d93ced0162a8eb5
MahdiDroid <mehdi.mhmvd@gmail.com>
2019-12-23 14:28:23 +0300

    added add-migration InitialModel

---
+++ YourQuoteReminder/Migrations/201912231127461_initialModel.cs
@@ -0,0 +1,27 @@
+﻿   add-migration Itinamespace YourQuoteReminder.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initialModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Quotes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserQuote = c.String(),
+                        CreatedDateTime = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Quotes");
+        }
+    }
+}
